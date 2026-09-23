1b62b147bcb85698fee42ca062aceaf251f8d026
Nada Ezz <nadaezzeldein6@gmail.com>
2026-09-23 07:25:01 +0300

    Question 1

--- OOP Assignmen_4/Program.cs
+++ OOP Assignmen_4/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
+            #region Question 1
+            // a)  What is Abstraction in Object-Oriented Programming?
+            //  simplifying complex systems by modeling classes based on the essential properties and behaviors of objects while hiding unnecessary details.
 
+            // b)  Why is abstraction considered one of the four pillars of OOP ?
+            //  because it allows developers to focus on the essential features of an object while ignoring irrelevant details.
+            #endregion
         }
     }
 }
