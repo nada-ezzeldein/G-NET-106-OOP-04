3f9253fb259a81401a1ada22feb95d66ef1553da
Nada Ezz <nadaezzeldein6@gmail.com>
2026-09-23 07:29:28 +0300

    Question 2

--- OOP Assignmen_4/Program.cs
+++ OOP Assignmen_4/Program.cs
@@ -11,6 +11,17 @@
             // b)  Why is abstraction considered one of the four pillars of OOP ?
             //  because it allows developers to focus on the essential features of an object while ignoring irrelevant details.
             #endregion
+
+            #region Question 2
+            //  a)  What is the difference between an Abstract Class and an Interface?
+            //  An abstract class can have both abstract and concrete methods, while an interface can only have abstract methods.
+
+            //  b)  When would you choose an Interface instead of an Abstract Class?
+            //  When you want to define a contract that multiple classes can implement, regardless of their inheritance hierarchy.
+
+            //  c)  Can a class inherit from multiple abstract classes? Can it implement multiple interfaces?
+            //  A class cannot inherit from multiple abstract classes, but it can implement multiple interfaces.
+            #endregion
         }
     }
 }
